a921b01efacf43cf0ec39f59cbf6faef9867a811
Simon Mezzomo <540861+simlt@users.noreply.github.com>
2025-01-15 15:16:05 +0100

    sync nvim config

--- dot_config/nvim/lua/plugins/simlt.lua
+++ dot_config/nvim/lua/plugins/simlt.lua
@@ -2,35 +2,10 @@ return {
   -- Configure LazyVim theme and config
   {
     "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-      flavour = "macchiato", -- latte, frappe, macchiato, mocha
-    },
-    -- opts = function()
-    --   -- Check if running in WSL
-    --   local is_wsl = vim.fn.has("wsl") == 1
-    --
-    --   -- if is_wsl then
-    --   --   -- Set clipboard provider to wsl-clipboard
-    --   --   vim.g.clipboard = {
-    --   --     name = "WslClipboard",
-    --   --     copy = {
-    --   --       ["+"] = "clip.exe",
-    --   --       ["*"] = "clip.exe",
-    --   --     },
-    --   --     paste = {
-    --   --       ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-    --   --       ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-    --   --     },
-    --   --     cache_enabled = 0,
-    --   --   }
-    --   -- end
-    --
-    --   return {
-    --     colorscheme = "catppuccin",
-    --     flavour = "macchiato", -- latte, frappe, macchiato, mocha
-    --   }
-    -- end,
+    -- opts = {
+    --   colorscheme = "catppuccin",
+    --   flavour = "macchiato", -- latte, frappe, macchiato, mocha
+    -- },
   },
   -- tokionight
   {
@@ -45,12 +20,40 @@ return {
     "echasnovski/mini.pairs",
     enabled = false,
   },
+  -- Disable window tabs
+  -- {
+  --   "akinsho/bufferline.nvim",
+  --   opts = {
+  --     options = {
+  --       mode = "tabs",
+  --     }
+  --   }
+  -- },
 
   -- Extra plugins
   {
+    { import = "lazyvim.plugins.extras.ai.codeium" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
-    -- { import = "lazyvim.plugins.extras.coding.codeium" },
+    { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.ui.treesitter-context" },
+  },
+  -- Neotest
+  {
+    { import = "lazyvim.plugins.extras.test.core" },
+    {
+      "nvim-neotest/neotest",
+      opts = {
+        adapters = {
+          "neotest-jest",
+          "neotest-vitest",
+        },
+      },
+      dependencies = {
+        "haydenmeade/neotest-jest",
+        "marilari88/neotest-vitest",
+      },
+      -- TODO: add patch from /home/sf51659/.local/share/nvim/lazy/neotest-jest/lua/neotest-jest/init.lua
+    },
   },
 
   -- Language support
@@ -58,7 +61,71 @@ return {
     -- TS/JS
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
+    -- { import = "lazyvim.plugins.extras.formatting.biome" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    -- JSON
+    { import = "lazyvim.plugins.extras.lang.json" },
     -- Vue
-    { import = "lazyvim.plugins.extras.lang.vue" },
+    -- { import = "lazyvim.plugins.extras.lang.vue" },
+    -- YAML
+    { import = "lazyvim.plugins.extras.lang.yaml" },
+    -- Docker
+    { import = "lazyvim.plugins.extras.lang.docker" },
+    -- Markdown
+    { import = "lazyvim.plugins.extras.lang.markdown" },
   },
+
+  -- configure conform to only use specific formatter for JS/TS files
+  {
+    "stevearc/conform.nvim",
+    opts = function()
+      return {
+        formatters = {
+          -- biome = {
+          --   prepend_args = {
+          --     -- use spaces
+          --     use_spaces = true,
+          --   }
+          -- },
+        },
+        formatters_by_ft = {
+          -- biome setup
+          -- javascript = { "biome" },
+          -- typescript = { "biome" },
+          -- typescriptreact = { "biome" },
+          -- javascriptreact = { "biome" },
+          -- vue = { "biome" },
+
+          -- prettier
+          javascript = { "prettierd" },
+          typescript = { "prettierd" },
+          -- typescriptreact = { "prettier" },
+          -- javascriptreact = { "prettier" },
+          -- vue = { "prettier" },
+        },
+      }
+    end,
+  },
+  -- Commented: this was probably solved by conform configuration
+  -- Make eslint behave properly with formatter (ignore vtsls)
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = {
+  --     -- servers = { eslint = {} },
+  --     setup = {
+  --       eslint = function()
+  --         require("lazyvim.util").lsp.on_attach(function(client)
+  --           if client.name == "eslint" then
+  --             client.server_capabilities.documentFormattingProvider = true
+  --             -- elseif client.name == "tsserver" or client.name == "vtsls" then
+  --             --   client.server_capabilities.documentFormattingProvider = false
+  --           end
+  --         end)
+  --       end,
+  --     },
+  --   },
+  --   -- config = function()
+  --   --   vim.lsp.set_log_level("debug")
+  --   -- end,
+  -- },
 }
